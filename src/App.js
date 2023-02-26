544dc942a4e91c08ebf69e79a4b80989ac68cf1b
splean101 <101splean@gmail.com>
2023-02-26 10:40:33 +0200

    done

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
     sugar: 0,
   });
 
-  const{coffe, sugar} = product
- 
+  const { coffe, sugar } = product;
+
   const save = () => {
     localStorage.setItem('coffe', coffe);
     localStorage.setItem('sugar', sugar);
@@ -16,16 +16,44 @@ function App() {
   const clear = () => {
     localStorage.removeItem('coffe');
     localStorage.removeItem('sugar');
-    setCoffe(0);
-    setSugar(0);
+    setProduct({ coffe: 0, sugar: 0 });
   };
 
   useEffect(() => {
     if (localStorage.getItem('coffe')) {
-      setCoffe(+localStorage.getItem('coffe'));
-      setSugar(+localStorage.getItem('sugar'));
+      setProduct({ ...product, coffe: +localStorage.getItem('coffe') });
+      setProduct({ ...product, coffe: +localStorage.getItem('sugar') });
     }
   }, []);
+
+  const removeCoffe = () => {
+    if (coffe > 0) {
+      setProduct({ ...product, coffe: product.coffe - 1 });
+    }
+  };
+
+  const removeSugar = () => {
+    if (sugar > 0) {
+      setProduct({ ...product, sugar: product.sugar - 1 });
+    }
+  };
+
+  const [delButton, setDelButton] = useState({
+    coffeButton: true,
+    sugarButton: true,
+  });
+
+  useEffect(() => {
+    coffe === 0
+      ? setDelButton({ ...delButton, coffeButton: false })
+      : setDelButton({ ...delButton, coffeButton: true });
+  }, [coffe]);
+
+  useEffect(() => {
+    sugar === 0
+      ? setDelButton({ ...delButton, sugarButton: false })
+      : setDelButton({ ...delButton, sugarButton: true });
+  }, [sugar]);
 
   return (
     <div className="wrapper">
@@ -33,13 +61,27 @@ function App() {
         <h1>Product list</h1>
         <div className="product">
           <span>{`Coffe: ${coffe}`}</span>
-          <button onClick={() => setProduct({...product, coffe: product.coffe + 1})}>Add</button>
-          <button onClick={() => setProduct({...product, coffe: product.coffe - 1})}>Remove</button>
+          <button
+            onClick={() =>
+              setProduct({ ...product, coffe: product.coffe + 1 })
+            }>
+            Add
+          </button>
+          {delButton.coffeButton && (
+            <button onClick={removeCoffe}>Remove</button>
+          )}
         </div>
         <div className="product">
           <span>{`Sugar: ${sugar}`}</span>
-          <button onClick={() => setProduct({...product, sugar: product.sugar + 1})}>Add</button>
-          <button onClick={() => setProduct({...product, sugar: product.sugar - 1})}>Remove</button>
+          <button
+            onClick={() =>
+              setProduct({ ...product, sugar: product.sugar + 1 })
+            }>
+            Add
+          </button>
+          {delButton.sugarButton && (
+            <button onClick={removeSugar}>Remove</button>
+          )}
         </div>
         <div className="save">
           <button onClick={save}>SAVE</button>
